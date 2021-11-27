fdca8a28bd501612d5143a230b740ab9a92f6b1a
unknown <rizkimuhammad2301@gmail.com>
2021-11-27 18:44:16 +0700

    Create API Checking User Email

--- handler/user.go
+++ handler/user.go
@@ -89,5 +89,52 @@ func (h *userHandler) Login(c *gin.Context) {
 		"Success", formatter)
 
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
+
+	var input user.CheckEmailInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+
+		errors := helper.FormatValidationError(err)
+
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse(
+			"Email Checking Failed", http.StatusUnprocessableEntity,
+			"Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	isEmailAvailable, err := h.userService.IsEmailAvailable(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": "Server Error"}
+
+		response := helper.APIResponse(
+			"Email Checking Failed", http.StatusUnprocessableEntity,
+			"Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	data := gin.H{
+		"is_available": isEmailAvailable,
+	}
+
+	metaMessage := "Email Has been registered"
+
+	if isEmailAvailable {
+		metaMessage = "Email is available"
+	}
+
+	response := helper.APIResponse(
+		metaMessage, http.StatusOK,
+		"Success", data)
+	c.JSON(http.StatusOK, response)
 
 }
